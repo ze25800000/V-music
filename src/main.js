a13caae3c8059eee7953e60d5874f406056dde0e
ze25800000 <ze258100000@sina.com>
2017-10-18 00:39:14 +0800

    3-2 路由配置+顶导组件开发

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 import fastclick from 'fastclick'
 import 'common/stylus/index.styl'
 
@@ -8,5 +9,6 @@ fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
