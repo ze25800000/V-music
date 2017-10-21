8ffb965c755874b49615c613c77c0c58f1bc49dd
ze25800000 <ze258100000@sina.com>
2017-10-21 20:33:14 +0800

    4-7 歌单数据接口分析

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -22,7 +22,7 @@
 
 <script type="text/ecmascript-6">
   import Slider from 'base/slider/slider'
-  import {getRecommend} from 'api/recommend'
+  import {getRecommend, getDiscList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
 
   export default {
@@ -33,6 +33,7 @@
     },
     created() {
       this._getRecommend()
+      this._getDiscList()
     },
     methods: {
       _getRecommend() {
@@ -40,6 +41,11 @@
           if (res.code === ERR_OK) {
             this.recommends = res.data.slider
           }
+        })
+      },
+      _getDiscList() {
+        getDiscList().then((res) => {
+          console.log(res)
         })
       }
     },
