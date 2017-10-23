2c827e640881506566f7ba332a87fde1bdc3e20d
ze25800000 <ze258100000@sina.com>
2017-10-23 17:18:23 +0800

    5-5 listview基础组件开发和应用-右侧快速入口实现（1）

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -11,3 +11,13 @@ export function hasClass(el, className) {
   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
   return reg.test(el.className)
 }
+
+export function getData(el, name, val) {
+  const prefix = 'data-'
+  let names = prefix + name
+  if (val) {
+    return el.setAttribute(names, val)
+  } else {
+    return el.getAttribute(names)
+  }
+}
