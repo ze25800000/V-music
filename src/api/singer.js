7d69019f11b6e93a20a8bba9b0ebcc998a046b26
ze25800000 <ze258100000@sina.com>
2017-10-29 21:50:26 +0800

    6-5 歌手详情数据抓取

--- src/api/singer.js
+++ src/api/singer.js
@@ -16,3 +16,18 @@ export function getSingerList() {
   })
   return jsonp(url, data, options)
 }
+
+export function getSingerDetail(singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    begin: 0,
+    num: 100,
+    songsstatus: 1,
+    singermid: singerId,
+    g_tk: 260023228
+  })
+  return jsonp(url, data, options)
+}
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -6,6 +6,8 @@
 
 <script type="text/ecmascript-6">
   import {mapGetters} from 'vuex'
+  import {getSingerDetail} from 'api/singer'
+  import {ERR_OK} from 'api/config'
 
   export default {
     computed: {
@@ -15,6 +17,22 @@
     },
     created() {
       console.log(this.singer)
+      this._getDetail()
+    },
+    methods: {
+      _getDetail() {
+        if (!this.singer.id) {
+          this.$router.push({
+            path: '/singer'
+          })
+          return
+        }
+        getSingerDetail(this.singer.id).then(res => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.list)
+          }
+        })
+      }
     }
   }
 </script>
