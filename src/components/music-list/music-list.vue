6596a43c3f99cb1568166755f33d2d35f411d61d
ze25800000 <ze258100000@sina.com>
2017-10-30 14:52:59 +0800

    7-3 播放器Vuex的相关应用

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -21,7 +21,10 @@
             class="list"
             ref="list">
       <div class="song-list-wrapper">
-        <song-list :songs="songs"></song-list>
+        <song-list
+          :songs="songs"
+          @select="selectItem"
+        ></song-list>
       </div>
       <div class="loading-container" v-show="!songs.length">
         <loading></loading>
@@ -35,6 +38,7 @@
   import SongList from 'base/song-list/song-list'
   import {prefixStyle} from 'common/js/dom'
   import Loading from 'base/loading/loading'
+  import {mapActions} from 'vuex'
 
   const transform = prefixStyle('transform')
   const backdrop = prefixStyle('backdrop-filter')
@@ -74,7 +78,16 @@
       },
       back() {
         this.$router.back()
-      }
+      },
+      selectItem(item, index) {
+        this.selectPlay({
+          list: this.songs,
+          index
+        })
+      },
+      ...mapActions([
+        'selectPlay'
+      ])
     },
     mounted() {
       this.imageHeight = this.$refs.bgImage.clientHeight
