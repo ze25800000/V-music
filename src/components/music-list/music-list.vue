13554a3a962194ada3ce578e7935ab54f8c30a40
ze25800000 <ze258100000@sina.com>
2017-10-30 11:10:17 +0800

    6-13 music-list组件开发（6）

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -24,7 +24,10 @@
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
   import SongList from 'base/song-list/song-list'
+  import {prefixStyle} from 'common/js/dom'
 
+  const transform = prefixStyle('transform')
+  const backdrop = prefixStyle('backdrop-filter')
   const RESERVED_HEIGHT = 40
   export default {
     props: {
@@ -68,8 +71,7 @@
     watch: {
       scrollY(newY) {
         let translateY = Math.max(this.minTranslateY, newY)
-        this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
-        this.$refs.layer.style['webkitTransform '] = `translate3d(0,${translateY}px,0)`
+        this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
         let zIndex = 0
         let scale = 1
         let blur = 0
@@ -80,8 +82,7 @@
         } else {
           blur = Math.min(20 * percent, 20)
         }
-        this.$refs.filter.style['backdrop-filter'] = `blur(${blur}px)`
-        this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur}px)`
+        this.$refs.filter.style[backdrop] = `blur(${blur}px)`
         if (newY < this.minTranslateY) {
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
@@ -91,8 +92,7 @@
           this.$refs.bgImage.style.height = 0
         }
         this.$refs.bgImage.style.zIndex = zIndex
-        this.$refs.bgImage.style['transform'] = `scale(${scale})`
-        this.$refs.bgImage.style['webkitTransform '] = `scale(${scale})`
+        this.$refs.bgImage.style[transform] = `scale(${scale})`
       }
     },
     components: {
