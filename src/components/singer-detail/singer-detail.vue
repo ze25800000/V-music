38103c8881e9736b443da413daa482881f8dd6c1
ze25800000 <ze258100000@sina.com>
2017-10-30 09:09:48 +0800

    6-8 music-list组件开发（1）

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,10 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list
+      :songs="song"
+      :title="title"
+      :bg-image="bgImage"
+    ></music-list>
   </transition>
 </template>
 
@@ -9,6 +13,7 @@
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
 
   export default {
     data() {
@@ -17,12 +22,17 @@
       }
     },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
     },
     created() {
-      console.log(this.singer)
       this._getDetail()
     },
     methods: {
@@ -36,7 +46,6 @@
         getSingerDetail(this.singer.id).then(res => {
           if (res.code === ERR_OK) {
             this.songs = this._normalizeSongs(res.data.list)
-            console.log(this.songs)
           }
         })
       },
@@ -50,20 +59,15 @@
         })
         return ret
       }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
 
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
