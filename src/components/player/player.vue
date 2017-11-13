2661a8c20d580c078158b51c37e0cda20fcaa11f
ze25800000 <ze258100000@sina.com>
2017-11-13 15:02:12 +0800

    7-22 播放器歌词左右滑动实现（上）

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -12,7 +12,11 @@
           <h1 class="title" v-html="currentSong.name"></h1>
           <h2 class="subtitle" v-html="currentSong.singer"></h2>
         </div>
-        <div class="middle">
+        <div class="middle"
+             @touchstart.prevent="middleTouchStart"
+             @touchmove.prevent="middleTouchMove"
+             @touchend.prevent="middleTouchEnd"
+        >
           <div class="middle-l">
             <div class="cd-wrapper">
               <div class="cd" :class="cdCls">
@@ -35,6 +39,10 @@
           </scroll>
         </div>
         <div class="bottom">
+          <div class="dot-wrapper">
+            <span class="dot" :class="{'active':currentShow==='cd'}"></span>
+            <span class="dot" :class="{'active':currentShow==='lyric'}"></span>
+          </div>
           <div class="progress-wrapper">
             <span class="time time-l">{{format(currentTime)}}</span>
             <div class="progress-bar-wrapper">
@@ -95,7 +103,9 @@
   import {shuffle} from 'common/js/util'
   import Lyric from 'lyric-parser'
   import Scroll from 'base/scroll/scroll'
+  import {prefixStyle} from 'common/js/dom'
 
+  let transform = prefixStyle('transform')
   export default {
     data() {
       return {
@@ -103,7 +113,8 @@
         currentTime: 0,
         radius: 32,
         currentLyric: null,
-        currentLineNum: 0
+        currentLineNum: 0,
+        currentShow: 'cd'
       }
     },
     computed: {
@@ -134,6 +145,9 @@
         'mode',
         'sequenceList'
       ])
+    },
+    created() {
+      this.touch = {}
     },
     methods: {
       back() {
@@ -240,7 +254,6 @@
           if (this.playing) {
             this.currentLyric.play()
           }
-          console.log(this.currentLyric)
         })
       },
       handleLyric({lineNum, txt}) {
@@ -251,6 +264,28 @@
         } else {
           this.$refs.lyricList.scrollToElement(0, 0, 1000)
         }
+      },
+      middleTouchStart(e) {
+        this.touch.initiated = true
+        const touch = e.touches[0]
+        this.touch.startX = touch.pageX
+        this.touch.startY = touch.pageY
+      },
+      middleTouchMove(e) {
+        if (!this.touch.initiated) {
+          return
+        }
+        const touch = e.touches[0]
+        const deltaX = touch.pageX - this.touch.startX
+        const deltaY = touch.pageY - this.touch.startY
+        if (Math.abs(deltaY) > Math.abs(deltaX)) {
+          return
+        }
+        const left = this.currentShow === 'cd' ? 0 : -window.innerWidth
+        const width = Math.min(Math.max(-window.innerWidth, left + deltaX))
+        this.$refs.lyricList.$el.style[transform] = `translate3d(${width}px,0,0)`
+      },
+      middleTouchEnd(e) {
       },
       ...mapMutations({
         setFullScreen: 'SET_FULL_SCREEN',
