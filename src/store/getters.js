119b10f97aab26b8084c1bb9b5c08269dc984fb1
ze25800000 <ze258100000@sina.com>
2017-11-14 19:17:52 +0800

    8-1 歌单详情页布局介绍Vuex实现路由数据通讯

--- src/store/getters.js
+++ src/store/getters.js
@@ -15,3 +15,4 @@ export const currentIndex = state => state.currentIndex
 export const currentSong = (state) => {
   return state.playList[state.currentIndex]
 }
+export const disc = state => state.disc
