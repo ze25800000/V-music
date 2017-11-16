09542281c6ccc9bf86fe18a6336dc008ca39da0f
ze25800000 <ze258100000@sina.com>
2017-11-16 09:03:13 +0800

    9-2 排行页排行榜数据应用

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="rank">
-    <div class="toplist">
+  <div class="rank" ref="rank">
+    <Scroll :data="topList" class="toplist" ref="toplist">
       <ul>
-        <li class="item">
+        <li class="item" v-for="item in topList">
           <div class="icon">
-            <img width="100" height="100">
+            <img width="100" height="100" v-lazy="item.picUrl">
           </div>
           <ul class="songlist">
-            <li class="song">
-              <span></span>
-              <span></span>
+            <li class="song" v-for="(song,index) in item.songList">
+              <span>{{index + 1}}</span>
+              <span>{{song.songname}}--{{song.singername}}</span>
             </li>
           </ul>
         </li>
       </ul>
-    </div>
+      <div class="loading-container" v-show="!topList.length">
+        <Loading></Loading>
+      </div>
+    </Scroll>
     <router-view></router-view>
   </div>
 </template>
@@ -22,8 +25,12 @@
 <script type="text/ecmascript-6">
   import {getTopList} from 'api/rank'
   import {ERR_OK} from 'api/config'
+  import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
+  import {playlistMixin} from 'common/js/mixin'
 
   export default {
+    mixins: [playlistMixin],
     data() {
       return {
         topList: []
@@ -33,6 +40,11 @@
       this._getTopList()
     },
     methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length ? '60px' : ''
+        this.$refs.rank.style.bottom = bottom
+        this.$refs.toplist.$el.refresh()
+      },
       _getTopList() {
         getTopList()
           .then(res => {
@@ -44,6 +56,10 @@
             console.log(err)
           })
       }
+    },
+    components: {
+      Scroll,
+      Loading
     }
   }
 </script>
