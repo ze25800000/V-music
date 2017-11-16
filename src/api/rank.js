c5f6bb3d7cca95c92d3b88ca6313a5a4866ae773
ze25800000 <ze258100000@sina.com>
2017-11-16 08:48:37 +0800

    9-1 排行榜页面布局介绍给排行榜数据抓取

---
+++ src/api/rank.js
@@ -0,0 +1,17 @@
+import {commonParams, options} from './config'
+import jsonp from 'common/js/jsonp'
+
+export function getTopList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg'
+  const data = Object.assign({}, commonParams, {
+    g_tk: 466451264,
+    inCharset: 'utf-8',
+    outCharset: 'utf-8',
+    notice: 0,
+    platform: 'h5',
+    needNewCode: 1,
+    _: 1510792490392,
+    format: 'jsonp'
+  })
+  return jsonp(url, data, options)
+}
