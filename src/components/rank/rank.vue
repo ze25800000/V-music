550e7cee146a01ba17a18fbdc5e9f4ae8b8ce944
ze25800000 <ze258100000@sina.com>
2017-11-16 09:21:10 +0800

    9-3 榜单详情也布局介绍及Vuex实现路由数据通讯

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -2,7 +2,7 @@
   <div class="rank" ref="rank">
     <Scroll :data="topList" class="toplist" ref="toplist">
       <ul>
-        <li class="item" v-for="item in topList">
+        <li class="item" v-for="item in topList" @click="selectItem(item)">
           <div class="icon">
             <img width="100" height="100" v-lazy="item.picUrl">
           </div>
@@ -28,6 +28,7 @@
   import Scroll from 'base/scroll/scroll'
   import Loading from 'base/loading/loading'
   import {playlistMixin} from 'common/js/mixin'
+  import {mapMutations} from 'vuex'
 
   export default {
     mixins: [playlistMixin],
@@ -40,10 +41,19 @@
       this._getTopList()
     },
     methods: {
+      ...mapMutations({
+        setTopList: 'SET_TOP_LIST'
+      }),
+      selectItem(item) {
+        this.$router.push({
+          path: `/rank/${item.id}`
+        })
+        this.setTopList(item)
+      },
       handlePlaylist(playlist) {
         const bottom = playlist.length ? '60px' : ''
         this.$refs.rank.style.bottom = bottom
-        this.$refs.toplist.$el.refresh()
+        this.$refs.toplist.refresh()
       },
       _getTopList() {
         getTopList()
---
+++ src/components/top-list/top-list.vue
@@ -0,0 +1,38 @@
+<template>
+  <transition name="slide">
+    <music-list :rank="rank" :title="title" :bg-image="bgImage" :songs="songs"></music-list>
+  </transition>
+</template>
+
+<script type="text/ecmascript-6">
+  import MusicList from 'components/music-list/music-list'
+  import {mapGetters} from 'vuex'
+
+  export default {
+    computed: {
+      ...mapGetters([
+        'topList'
+      ]),
+      rank() {
+        return this.topList.rank
+      },
+      title() {
+        return this.topList.topTitle
+      },
+      bgImage() {
+        return this.topList.picUrl
+      }
+    },
+    components: {
+      MusicList
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .slide-enter-active, .slide-leave-active
+    transition: all 0.3s ease
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
+</style>
