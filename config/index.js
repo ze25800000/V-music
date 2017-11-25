a9256faf582fd805e3b3dc6ac7e5c2697197116a
ze25800000 <ze258100000@sina.com>
2017-11-25 20:24:02 +0800

    13-2 编译打包-项目编译打包及node服务

--- config/index.js
+++ config/index.js
@@ -1,4 +1,3 @@
-
 'use strict'
 // Template version: 1.1.3
 // see http://vuejs-templates.github.io/webpack for documentation.
@@ -8,6 +7,7 @@ const path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
+    port: 9000,
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
